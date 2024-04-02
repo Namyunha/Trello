2ae67a945e9879c3df98614a48c7fbc26189fa7f
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-04-02 21:06:03 +0900

    drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ function App() {
   return <DragDropContext onDragEnd={onDragEnd}>
     <div>
       <Droppable droppableId='one'>
-        {() => 
-          <ul>
+        {(magic) => (
+          <ul ref={magic.innerRef} {...magic.droppableProps}>
             <Draggable draggableId='first' index={0}>
-              {() => <li>One</li>}
+              {(magic) => (
+              <li ref={magic.innerRef} {...magic.draggableProps} 
+              >
+                <span {...magic.dragHandleProps}>🧲</span>
+                One
+            </li>)}
             </Draggable>
-            <Draggable draggableId='second' index={1}>
-              {() => <li>Two</li>}
-            </Draggable>
-          </ul>}
+          </ul>
+          )}
       </Droppable>
     </div>
   </DragDropContext>
