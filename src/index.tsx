7fb0464475fa8873bf4abda000bef1179c8a3ae1
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-04-02 21:39:47 +0900

    테마설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from "styled-components";
+import { colorTheme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <ThemeProvider theme={colorTheme}>
+    <GlobalStyle />
     <App />
+  </ThemeProvider>
 );
