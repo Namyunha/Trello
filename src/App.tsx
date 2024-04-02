490242a808531ac67496fa8a51776938648aff00
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-04-02 22:18:43 +0900

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  max-width: 480px;
-  width: 100%;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Boards = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
-`;
-
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
+import {
+  Wrapper,
+  Boards,
+  Board,
+  Card } 
+from "./styles"
 
 const toDos = ["a", "b", "c", "d", "e", "f"];
 
@@ -56,7 +30,7 @@ function App() {
                     )}
                   </Draggable>
                 ))}
-                {/* {magic.placeholder} */}
+                {magic.placeholder}
               </Board>
             )}
           </Droppable>
---
+++ src/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+export const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+export const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
